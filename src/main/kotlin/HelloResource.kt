e96bd7cfd086f1eed90b1a9a8eb373389001228a
Lucas Giori <lucasgioricesconetto@gmail.com>
2023-11-17 15:52:44 -0300

    env: Configurando ambiente de desenvolvimento

--- src/main/kotlin/HelloResource.kt
+++ src/main/kotlin/HelloResource.kt
@@ -2,13 +2,14 @@ import jakarta.ws.rs.GET
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
+import jakarta.ws.rs.core.Response
+import jakarta.ws.rs.core.Response.Status.OK
 
 @Path("/hello")
 class HelloResource {
-
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    fun hello(): String {
-        return "Hello, World!"
+    @Produces(MediaType.APPLICATION_JSON)
+    fun hello(): Response {
+        return Response.status(OK).entity(mapOf("message" to "Hello!!")).build()
     }
 }
