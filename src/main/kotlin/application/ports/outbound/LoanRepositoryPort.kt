71d4057987b3d4b493d1ccf6f90cb5a50c8da435
Lucas Giori <lucasgioricesconetto@gmail.com>
2024-05-26 22:53:21 -0300

    fix: Ajustando o tipo do retorno para retornar o agregado ao invés do evento

--- src/main/kotlin/application/ports/outbound/LoanRepositoryPort.kt
+++ src/main/kotlin/application/ports/outbound/LoanRepositoryPort.kt
@@ -3,10 +3,11 @@ package application.ports.outbound
 import application.domain.events.LoanEvent
 import application.domain.events.LoanInitializedEvent
 import application.domain.models.LoanId
+import application.domain.models.aggregate.Loan
 
 interface LoanRepositoryPort {
     /** @todo LoanEvent? */
-    suspend fun pull(loanId: LoanId): LoanEvent?
+    suspend fun pull(loanId: LoanId): Loan?
 
     suspend fun push(event: LoanInitializedEvent)
 
