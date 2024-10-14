750e006f2643ee36fa417094746b3630f34a3caa
Lucas Giori <lucasgioricesconetto@gmail.com>
2024-10-13 22:02:01 -0300

    feat: Ajustando serialização de BigDecimal

--- src/main/kotlin/starter/serializer/BigDecimalSerializer.kt
+++ src/main/kotlin/starter/serializer/BigDecimalSerializer.kt
@@ -7,16 +7,17 @@ import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import java.math.BigDecimal
+import java.math.RoundingMode
 
 object BigDecimalSerializer : KSerializer<BigDecimal> {
     override val descriptor: SerialDescriptor =
         PrimitiveSerialDescriptor("BigDecimal", PrimitiveKind.STRING)
 
     override fun serialize(encoder: Encoder, value: BigDecimal) {
-        encoder.encodeString(value.toString())
+        encoder.encodeString(value.setScale(2, RoundingMode.HALF_EVEN).toString())
     }
 
     override fun deserialize(decoder: Decoder): BigDecimal {
-        return BigDecimal(decoder.decodeString())
+        return BigDecimal(decoder.decodeString()).setScale(2, RoundingMode.HALF_EVEN)
     }
 }
